a06d688c7bd78d5373b9f14e874688f87e15da98
Hery Ruiz <149633842+HeryRuiz@users.noreply.github.com>
2024-02-08 10:27:40 -0500

    Update Text.jsx

--- src/pages/Home/Text.jsx
+++ src/pages/Home/Text.jsx
@@ -11,7 +11,7 @@ function Text() {
             <p>MARKET</p>
           </div>
           <h2>
-            Buy Bitcoin, Ethereum, and <br /> 49+ <span>Cryptocurrencies.</span>
+            Buy Bitcoin, Solana, and <br /> 49+ <span>Cryptocurrencies.</span>
           </h2>
         </div>
       </section>
