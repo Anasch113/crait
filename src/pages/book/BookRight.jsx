a5082c4c9cdedfae231c3e4ef64403c2a281cb40
hery Ruiz <heryrdev@gmail.com>
2024-02-22 17:32:26 -0500

    added images to a folder in book folder

--- src/pages/book/BookRight.jsx
+++ src/pages/book/BookRight.jsx
@@ -1,9 +1,14 @@
 import "./styles/BookRight.css";
+import chat from './images/chat.png'
+import micro from './images/mircro.png'
+import meta from './images/meta.png'
+import coh from './images/coh.png'
+import car from './images/car.png'
 function BookRight() {
   return (
     <div className="book__right">
       <img
-        src="https://cdn.discordapp.com/attachments/1187219037537714220/1196571942866649128/image.png?ex=65b81d76&is=65a5a876&hm=01e894e3541f255b01ccc564c15250780e1be94838b963cadd93cd40ae62da23&"
+        src={chat}
         alt="Open AI Logo"
       />
       <p className="book__des">
@@ -20,20 +25,20 @@ function BookRight() {
         <p>Trusted by the world's most ambitious AI teams</p>
         <div className="book__logo__grid">
           <img
-            src="https://cdn.discordapp.com/attachments/1187219037537714220/1196571964748353586/image.png?ex=65b81d7b&is=65a5a87b&hm=0b990a507b9108cd7bee01ddfb347e2fea85b794da215a6a65506c92b43a2afe&"
+            src={micro}
             alt="Microsoft Logo"
           />
           <img
-            src="https://cdn.discordapp.com/attachments/1187219037537714220/1196572114992508968/image.png?ex=65b81d9f&is=65a5a89f&hm=ca193adbeaf39944b284b7da561ff06e8e3ea88a9a091695550fb1f1e2aeeb31&"
+            src={car}
             alt="CarperAI Logo"
           />
           <img
-            src="https://cdn.discordapp.com/attachments/1187219037537714220/1196572181757431901/image.png?ex=65b81daf&is=65a5a8af&hm=a3d7d698ac66e750f6978429e0fb4c050b79fa2dbfdfc1a3c07da7531d0fe873&"
+            src={meta}
             alt="Meta Logo"
           />
           <img
-            src="https://cdn.discordapp.com/attachments/1187219037537714220/1196572275269443654/image.png?ex=65b81dc5&is=65a5a8c5&hm=ef9297b46312ffc550f07430f45bf559685e7e2d59bcb41da68e9e2c9b7b2da1&"
-            alt="Cohere Logo"
+            src={coh}
+            alt="Coher Logo"
           />
         </div>
       </div>
