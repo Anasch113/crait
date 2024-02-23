e1c4bc6a43a6b1a96eb6ffdd242a3c18433f99e5
hery Ruiz <heryrdev@gmail.com>
2024-02-23 03:05:28 -0500

    completed dash nav

--- src/pages/dashboard/DashNav.jsx
+++ src/pages/dashboard/DashNav.jsx
@@ -1,28 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import "./styles/DashNav.css";
-import logo from './images/logo.png'
+import logo from "./images/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngleDown,
+  faRightFromBracket,
+  faAngleUp,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 function DashNav() {
-  return (
-    <nav className='dashnav' id='dashNav'>
-        <div className='nav__container'>
-            <div className='nav__left'>
-                <div className='nav__left__logo'>
-                    <img src={logo} alt="YouCoin Logo" />
-                    <p className='nav__left__logo__name'>YouCoin</p>
-                    <p className='nav__beta'>BETA</p>
-                </div>  
-            </div>
-            <div className='nav__right'>
-                <div className='nav__right__flex'>
-                    <img src="https://cloud.appwrite.io/v1/avatars/initials?name=Hery+Ruiz&width=80&height=80&project=console" alt="Avatar" />
-                    <div className='nav__right__info'>
-                        <p>first last</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </nav>
+  const [dropdown, setDropDown] = useState(false);
+  useEffect(()=>{
+    if (dropdown === true) {
+        document.querySelector(".dropdown").style.display = "block";
+      } else {
+        document.querySelector(".dropdown").style.display = "none";
+      }
+  }, [dropdown]
   )
+ 
+  return (
+    <>
+      <nav className="dashnav" id="dashNav">
+        <div className="dashnav__container">
+          <div className="dashnav__left">
+            <div className="dashnav__left__logo">
+              <img src={logo} alt="YouCoin Logo" />
+              <p className="dashnav__left__logo__name">YouCoin</p>
+              <p className="dashnav__beta">BETA</p>
+            </div>
+          </div>
+          <div className="dashnav__right">
+            <div className="dashnav__right__flex">
+              <img
+                src="https://cloud.appwrite.io/v1/avatars/initials?name=Hery+Ruiz&width=80&height=80&project=console"
+                alt="Avatar"
+              />
+              <div className="dashnav__right__info">
+                <p className="dashnav__right__name">First Last</p>
+                <p className="dashnav__right__type">All Investments</p>
+              </div>
+              <FontAwesomeIcon
+                icon={dropdown ? faAngleUp : faAngleDown}
+                className="dashnav__drop"
+                onClick={() => setDropDown(!dropdown)}
+              />
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div className="dropdown__div">
+        <div className="dropdown">
+          <Link to="/" className="dropdown__item">
+            <p>Sign Out</p>
+            <FontAwesomeIcon icon={faRightFromBracket} />
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default DashNav
+export default DashNav;
