7439abff830101ee8ae8f1c4ea3ce1164758c008
hery Ruiz <heryrdev@gmail.com>
2024-02-23 16:32:25 -0500

    added bottom dash

--- src/pages/dashboard/DashBottom.jsx
+++ src/pages/dashboard/DashBottom.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
 import "./styles/bottomdash.css";
-const DashBottom = () => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus,faChevronLeft ,faChevronRight, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+function DashBottom() {
   return (
-    <div>DashBottom</div>
-  )
+    <section className="dashbottom" id="dashbottom">
+      <div className="dash__content">
+        <div className="dashbottom__container">
+          <div className="dashbottom__one">
+            <p>Investments</p>
+            <Link to='/demo' className="dashbottom__purchase">
+              <FontAwesomeIcon icon={faPlus} />
+              Purchase
+            </Link>
+          </div>
+          <div className="dashbottom__two">
+            <div className="dashbottom__box">
+              <div className="dashbottom__box__text">
+                <span>NO INVESTMENTS</span>
+                <p>Username</p>
+              </div>
+              <p className="dashbottom__box__location">Boston</p>
+            </div>
+            <div className="dashbottom__box">
+              <div className="dashbottom__box__text">
+                <span>NO INVESTMENTS</span>
+                <p>Username</p>
+              </div>
+              <p className="dashbottom__box__location">Boston</p>
+            </div>
+          </div>
+          <div className="dashbottom__three">
+                <button className="dashbottom__selector">
+                    1
+                    <FontAwesomeIcon icon={faAngleDown} className="dashbottom__icon"/>
+                    
+                </button>
+                <div className="dashbottom__three__options">
+                    <div className="dashbottom__option">
+                    <FontAwesomeIcon icon={faChevronLeft} className="dashbottom__icon"/>
+                    <p>Prev</p>
+                    </div>
+                    <button disabled>1</button>
+                    <div className="dashbottom__option">
+                    <p>Next</p>
+                    <FontAwesomeIcon icon={faChevronRight} className="dashbottom__icon"/>
+                    </div>
+                </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default DashBottom
+export default DashBottom;
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import DashNav from './DashNav'
 import DashFooter from './DashFooter'
 import DashTop from './DashTop'
+import DashBottom from './DashBottom'
 
 function Dashboard() {
   return (
     <>
     <DashNav />
     <DashTop />
-    
+    <DashBottom />
     <DashFooter />
     </>
   )
