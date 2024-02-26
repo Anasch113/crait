3066cb8ccba30ad9635922c3633f16420807c7a7
hery Ruiz <heryrdev@gmail.com>
2024-02-26 18:13:18 -0500

    fixed initialization

--- src/pages/firebase/firebase.js
+++ src/pages/firebase/firebase.js
@@ -1,15 +1,16 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyARkDELtMZLcVEsxGZsQTIyKNiWFuoX-ww",
-    authDomain: "youcoin-60a70.firebaseapp.com",
-    projectId: "youcoin-60a70",
-    storageBucket: "youcoin-60a70.appspot.com",
-    messagingSenderId: "121578535180",
-    appId: "1:121578535180:web:774e66110a9d8c3a173129"
+  apiKey: "AIzaSyARkDELtMZLcVEsxGZsQTIyKNiWFuoX-ww",
+  authDomain: "youcoin-60a70.firebaseapp.com",
+  projectId: "youcoin-60a70",
+  storageBucket: "youcoin-60a70.appspot.com",
+  messagingSenderId: "121578535180",
+  appId: "1:121578535180:web:774e66110a9d8c3a173129"
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
 
-export const auth = firebase.auth();
+export {auth, app}
