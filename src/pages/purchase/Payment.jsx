ab46fda17130a3e92f4eb652a9bef1735ea78431
hery Ruiz <heryrdev@gmail.com>
2024-02-28 20:00:59 -0500

    added functionality to the divs

--- src/pages/purchase/Payment.jsx
+++ src/pages/purchase/Payment.jsx
@@ -1,18 +1,27 @@
-import React from "react";
 import "./styles/Payment.css";
+import React, { useState } from "react";
 function Payment() {
+  const [code, setCode] = useState(true);
   return (
     <div className="payment__container">
-      <div className="payment__code">
+      <div className="payment__code" onClick={() => setCode(true)}>
         <div className="payment__flex">
-          <div className="purchase__box__button__sel"></div>
+          <div
+            className={
+              code ? "purchase__box__button__sel" : "purchase__box__button__not"
+            }
+          ></div>
           <p className="purchase__box__title2">Pro - 1 Month Free</p>
         </div>
       </div>
-      <div className="payment__new">
+      <div className="payment__new" onClick={() => setCode(false)}>
         <div className="payment__flex">
-          <div className="purchase__box__button__not"></div>
-          <p className="purchase__box__title2">Pro - 1 Month Free</p>
+          <div
+            className={
+              code ? "purchase__box__button__not" : "purchase__box__button__sel"
+            }
+          ></div>
+          <p className="purchase__box__title2">Add new payment method</p>
         </div>
       </div>
     </div>
