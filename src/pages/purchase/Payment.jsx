cc0a57c9747e04fe3a309b327382b65fd2796fb8
hery Ruiz <heryrdev@gmail.com>
2024-02-28 19:36:32 -0500

    added styling to divs

--- src/pages/purchase/Payment.jsx
+++ src/pages/purchase/Payment.jsx
@@ -3,8 +3,18 @@ import "./styles/Payment.css";
 function Payment() {
   return (
     <div className="payment__container">
-      <div className="payment__code"></div>
-      <div className="payment__new"></div>
+      <div className="payment__code">
+        <div className="payment__flex">
+          <div className="purchase__box__button__sel"></div>
+          <p className="purchase__box__title2">Pro - 1 Month Free</p>
+        </div>
+      </div>
+      <div className="payment__new">
+        <div className="payment__flex">
+          <div className="purchase__box__button__not"></div>
+          <p className="purchase__box__title2">Pro - 1 Month Free</p>
+        </div>
+      </div>
     </div>
   );
 }
