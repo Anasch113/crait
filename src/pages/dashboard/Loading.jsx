3839ddd7df5db0e07aa3f040690b0db21a720dad
hery Ruiz <heryrdev@gmail.com>
2024-03-06 06:41:25 -0500

    made the loader better

--- src/pages/dashboard/Loading.jsx
+++ src/pages/dashboard/Loading.jsx
@@ -1,82 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/Loading.css";
+import { BarLoader } from "react-spinners";
+import logo from "./images/logo.png";
+
 function Loading() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
-      <div className="loading__opacity">
+      
         <div className="loading__div">
-          <nav className="dashnav" id="dashNav">
-            <div className="dashnav__container">
-              <div className="dashnav__left">
-                <div className="dashnav__left__logo"></div>
-              </div>
-              <div className="dashnav__right">
-                <div className="dashnav__right__container">
-                  <div className="dashnav__right__flex">
-                    <div className="dashnav__right__info">
-                      <p className="dashnav__right__name capitalize"></p>
-
-                      <p className="dashnav__right__type"></p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </nav>
-          <section className="dashtop" id="dashtop">
-            <div className="dashtop__container">
-              <div className="dashtop__top">
-                <div className="dashtop__left">
-                  <p className="dashtop__title"></p>
-                </div>
-                <div className="dashtop__right"></div>
-              </div>
-              <div className="dashtop__bottom">
-                <div className="dashtop__options">
-                  <p></p>
-                  <p></p>
-                </div>
-              </div>
-            </div>
-          </section>
-          <section className="dashbottom" id="dashbottom">
-            <div className="dash__content">
-              <div className="dashbottom__container">
-                <div className="dashbottom__one">
-                  <p></p>
-                </div>
-                <div className="dashbottom__two">
-                  <div className="dashbottom__box">
-                    <div className="dashbottom__box__text">
-                      <span></span>
-                      <p></p>
-                    </div>
-                    <p className="dashbottom__box__location"></p>
-                  </div>
-                  <div className="dashbottom__box">
-                    <div className="dashbottom__box__text">
-                      <span></span>
-                      <p></p>
-                    </div>
-                    <p className="dashbottom__box__location"></p>
-                  </div>
-                </div>
-                <div className="dashbottom__three">
-                  <div className="dashbottom__three__options">
-                    <div className="dashbottom__option">
-                      <p></p>
-                    </div>
-                    <div className="dashbottom__option">
-                      <p></p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-          <div className="dash__filler"></div>
+          <div className="loading__flex">
+            <img src={logo} alt="Logo" />
+            {windowWidth > 400? (
+            <BarLoader
+              color="#E688A6"
+              height="10px"
+              width="300px"
+              speedMultiplier="1.5"
+            />
+            ): (
+              <BarLoader
+              color="#E688A6"
+              height="10px"
+              width="200px"
+              speedMultiplier="1.5"
+            />
+            )
+          }
+          </div>
         </div>
-      </div>
+      
     </>
   );
 }
