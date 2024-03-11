1cb0220a2cc294d485c468e5b30d4a920e8b2530
hery Ruiz <heryrdev@gmail.com>
2024-03-11 11:53:35 -0400

    added links

--- src/pages/Layout/Support.jsx
+++ src/pages/Layout/Support.jsx
@@ -26,35 +26,63 @@ function Support({ setOpen, setArrowStyle, resetArrowStyle }) {
       <div className="support__middle">
         <p>Content</p>
         <div className="support__grid">
-          <Link to="/dashboard" className="support__item">
+          <Link
+            to="/dashboard"
+            className="support__item"
+            onClick={() => setOpen(false)}
+          >
             <img src={data} alt="Dashboard" />
             <p className="support__subject">Dashboard</p>
           </Link>
-          <Link to="/services" className="support__item">
+          <Link
+            to="/services"
+            className="support__item"
+            onClick={() => setOpen(false)}
+          >
             <img src={cir} alt="Dashboard" />
             <p className="support__subject">Services</p>
           </Link>
-          <Link to="/purchase" className="support__item">
+          <Link
+            to="/purchase"
+            className="support__item"
+            onClick={() => setOpen(false)}
+          >
             <img src={octo} alt="Dashboard" />
             <p className="support__subject">Purchase</p>
           </Link>
-          <Link to="/demo" className="support__item">
+          <Link
+            to="/demo"
+            className="support__item"
+            onClick={() => setOpen(false)}
+          >
             <img src={speed} alt="Dashboard" />
             <p className="support__subject">Demo</p>
           </Link>
-          <Link to="/terms" className="support__item">
+          <Link
+            to="/terms"
+            className="support__item"
+            onClick={() => setOpen(false)}
+          >
             <img src={tool} alt="Dashboard" />
             <p className="support__subject">Terms</p>
           </Link>
-          <Link to="/" className="support__item">
+          <Link to="/" className="support__item" onClick={() => setOpen(false)}>
             <img src={basket} alt="Dashboard" />
             <p className="support__subject">Market</p>
           </Link>
-          <Link to="/signup" className="support__item">
+          <Link
+            to="/signup"
+            className="support__item"
+            onClick={() => setOpen(false)}
+          >
             <img src={multi} alt="Dashboard" />
             <p className="support__subject">Sign up</p>
           </Link>
-          <Link to="/login" className="support__item">
+          <Link
+            to="/login"
+            className="support__item"
+            onClick={() => setOpen(false)}
+          >
             <img src={light} alt="Dashboard" />
             <p className="support__subject">Login</p>
           </Link>
