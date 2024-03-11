dc68e7595148123d11f054cfabbd4fad7c111cb1
hery Ruiz <heryrdev@gmail.com>
2024-03-11 11:59:46 -0400

    moved button to the bottom

--- src/pages/Layout/Support.jsx
+++ src/pages/Layout/Support.jsx
@@ -14,78 +14,84 @@ import basket from "../Layout/images/basket.png";
 function Support({ setOpen, setArrowStyle, resetArrowStyle }) {
   return (
     <div className="support" id="support">
-      <div className="support__top">
-        <div></div>
-        <p className="support__title">Support Center</p>
-        <FontAwesomeIcon
-          icon={faXmark}
-          className="support__close"
-          onClick={() => setOpen(false)}
-        />
-      </div>
-      <div className="support__middle">
-        <p>Content</p>
-        <div className="support__grid">
-          <Link
-            to="/dashboard"
-            className="support__item"
+      <div>
+        <div className="support__top">
+          <div></div>
+          <p className="support__title">Support Center</p>
+          <FontAwesomeIcon
+            icon={faXmark}
+            className="support__close"
             onClick={() => setOpen(false)}
-          >
-            <img src={data} alt="Dashboard" />
-            <p className="support__subject">Dashboard</p>
-          </Link>
-          <Link
-            to="/services"
-            className="support__item"
-            onClick={() => setOpen(false)}
-          >
-            <img src={cir} alt="Dashboard" />
-            <p className="support__subject">Services</p>
-          </Link>
-          <Link
-            to="/purchase"
-            className="support__item"
-            onClick={() => setOpen(false)}
-          >
-            <img src={octo} alt="Dashboard" />
-            <p className="support__subject">Purchase</p>
-          </Link>
-          <Link
-            to="/demo"
-            className="support__item"
-            onClick={() => setOpen(false)}
-          >
-            <img src={speed} alt="Dashboard" />
-            <p className="support__subject">Demo</p>
-          </Link>
-          <Link
-            to="/terms"
-            className="support__item"
-            onClick={() => setOpen(false)}
-          >
-            <img src={tool} alt="Dashboard" />
-            <p className="support__subject">Terms</p>
-          </Link>
-          <Link to="/" className="support__item" onClick={() => setOpen(false)}>
-            <img src={basket} alt="Dashboard" />
-            <p className="support__subject">Market</p>
-          </Link>
-          <Link
-            to="/signup"
-            className="support__item"
-            onClick={() => setOpen(false)}
-          >
-            <img src={multi} alt="Dashboard" />
-            <p className="support__subject">Sign up</p>
-          </Link>
-          <Link
-            to="/login"
-            className="support__item"
-            onClick={() => setOpen(false)}
-          >
-            <img src={light} alt="Dashboard" />
-            <p className="support__subject">Login</p>
-          </Link>
+          />
+        </div>
+        <div className="support__middle">
+          <p>Content</p>
+          <div className="support__grid">
+            <Link
+              to="/dashboard"
+              className="support__item"
+              onClick={() => setOpen(false)}
+            >
+              <img src={data} alt="Dashboard" />
+              <p className="support__subject">Dashboard</p>
+            </Link>
+            <Link
+              to="/services"
+              className="support__item"
+              onClick={() => setOpen(false)}
+            >
+              <img src={cir} alt="Dashboard" />
+              <p className="support__subject">Services</p>
+            </Link>
+            <Link
+              to="/purchase"
+              className="support__item"
+              onClick={() => setOpen(false)}
+            >
+              <img src={octo} alt="Dashboard" />
+              <p className="support__subject">Purchase</p>
+            </Link>
+            <Link
+              to="/demo"
+              className="support__item"
+              onClick={() => setOpen(false)}
+            >
+              <img src={speed} alt="Dashboard" />
+              <p className="support__subject">Demo</p>
+            </Link>
+            <Link
+              to="/terms"
+              className="support__item"
+              onClick={() => setOpen(false)}
+            >
+              <img src={tool} alt="Dashboard" />
+              <p className="support__subject">Terms</p>
+            </Link>
+            <Link
+              to="/"
+              className="support__item"
+              onClick={() => setOpen(false)}
+            >
+              <img src={basket} alt="Dashboard" />
+              <p className="support__subject">Market</p>
+            </Link>
+            <Link
+              to="/signup"
+              className="support__item"
+              onClick={() => setOpen(false)}
+            >
+              <img src={multi} alt="Dashboard" />
+              <p className="support__subject">Sign up</p>
+            </Link>
+            <Link
+              to="/login"
+              className="support__item"
+              onClick={() => setOpen(false)}
+            >
+              <img src={light} alt="Dashboard" />
+              <p className="support__subject">Login</p>
+            </Link>
+          </div>
         </div>
       </div>
       <div className="support__bottom">
